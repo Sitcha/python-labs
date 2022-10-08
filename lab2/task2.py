70fe3fe7756b913b53a74c23443af95665d71ea4
Sitcha <sitchaaudrey@mail.com>
2022-10-08 07:53:00 -0700

    lab2 task2

--- lab2/task2.py
+++ lab2/task2.py
@@ -1,5 +1,6 @@
 num1 = input("type a number>")
-num2 = input("type another number>")
+num1 = int(num1)
+num2 = int(input("type another number>"))
 
 if num1 < num2 :
     print(f"{num1} is the smallest")
@@ -7,3 +8,4 @@ elif num2 < num1 :
     print(f"{num2} is the smallest")
 else:
     print(f"{num1} = {num2}")
+
