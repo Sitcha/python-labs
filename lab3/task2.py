61164a4aeb47ac6607251bcd9b354d531aa1fd8d
Sitcha <sitchaaudrey@mail.com>
2022-10-13 13:21:36 -0700

    exercise2

--- lab3/task2.py
+++ lab3/task2.py
@@ -4,6 +4,7 @@ lst = []
 
 for i in range(10):
     num =int(input("type a number :"))
-    lst.count(num)
- 
+    lst.append(num)
+    len([i for i in list if i == 0 ]) 
+
 print(f"nunmbers of number = 0 is {unique()}")
--- lab3/task3.py
+++ lab3/task3.py
@@ -1,6 +1,8 @@
 n = int(input('type a natural number :'))
 
 for i in range(1,n+1):
-   for g in range(1,i+1):
-    print(g, end=' ')
-print( )
+    for g in range(1,i+1):
+        print( g, end = "" )
+    print()
+
+
