dc1babc47cc5e2712577fdaeea8b2e77dd6db9f7
Sitcha <113554538+Sitcha@users.noreply.github.com>
2022-10-13 13:45:50 -0700

    Delete task4.py

--- lab3/task4.py
+++
@@ -1,9 +0,0 @@
-n = int(input('type a natural number :'))
-
-for i in range(1,n+1):
-    for x in range(1,i+1):
-     print(x,end='')
-     
-    for k in range(i-1,0,-1):
-     print(k,end='')
-print( )
